14f40349a236ed5ab4ef5733af7443e1981b2424
Simon Okbagergis Manna <32343117+saimenogm@users.noreply.github.com>
2025-03-18 12:22:30 +0300

    fourth-commit11

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 // app/api/users/route.ts
 import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@supabase/supabase-js";
 
 export async function POST(request: Request) {
   try {
